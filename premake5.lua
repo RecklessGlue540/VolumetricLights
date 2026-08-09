178e01e9c21c16a8b946f434840535013b7950ab
RecklessGlue540 <143945383+RecklessGlue540@users.noreply.github.com>
2026-08-09 13:47:22 +0300

    Switch to premake

---
+++ premake5.lua
@@ -0,0 +1,32 @@
+workspace "VolumetricLights"
+   configurations { "Release", "Debug" }
+   architecture "x86"
+   location "build"
+   cppdialect "C++latest"
+   kind "SharedLib"
+   language "C++"
+   targetdir "bin/%{cfg.buildcfg}"
+   targetextension ".asi"
+   buildoptions { "/dxifcInlineFunctions- /Zc:__cplusplus /utf-8" }
+   staticruntime "On"
+   characterset ("Unicode")
+   multiprocessorcompile ("On")
+   startproject "VolumetricLights"
+
+   defines { "_CRT_SECURE_NO_WARNINGS" }
+
+   includedirs { "source" }
+   includedirs { "source/dependencies" }
+   files { "source/**.h", "source/*.hpp", "source/*.cpp", "source/*.hxx", "source/**.ixx" }
+
+   includedirs { "dependencies/injector/safetyhook/include" }
+   includedirs { "dependencies/injector/zydis" }
+   includedirs { "dependencies/Hooking.Patterns" }
+   includedirs { "dependencies/injector/include" }
+   includedirs { "dependencies/IniReader" }
+   files { "dependencies/Hooking.Patterns/Hooking.Patterns.h", "dependencies/Hooking.Patterns/Hooking.Patterns.cpp" }
+   files { "dependencies/injector/safetyhook/include/**.hpp", "dependencies/injector/safetyhook/src/**.cpp" }
+   files { "dependencies/injector/zydis/**.h", "dependencies/injector/zydis/**.c" }
+   files { "resources/*.ini" }
+
+project "VolumetricLights"
